2c0fdf86eaf6511e45a51ec9c6320b2373ce2364
Jiwei,Yuan <ji-weiyuan@outlook.com>
2021-06-21 20:38:36 +0800

    test(QueueArray): add QueueArray's test

--- src/collections/Queue.ts
+++ src/collections/Queue.ts
@@ -1,6 +1,8 @@
 /**
  * Queue ADT
  * TODO: How to differentiate interface and class implement file
+ * Reference:
+ * 1.https://github.com/google/closure-library/blob/master/closure/goog/structs/queue.js
  */
 export interface Queue<Item> {
   /**
@@ -10,18 +12,28 @@ export interface Queue<Item> {
   enqueue(item: Item): number;
 
   /**
-   * Retrieves an removes the head of this queue.
+   * Retrieves and removes the head of this queue.
    * @return Item The item at the head of this queue. Returns undefined if the
    *     queue is empty.
    */
   dequeue(): Item | undefined;
 
   /**
-   *
+   * Retrieves but not remove the head of this queue.
+   * @return Item The element at the head of this queue. Return undefined if the
+   *    queue is empty
    */
   peek(): Item | undefined;
 
+  /**
+   * Returns the number of elements in this queue.
+   * @return {number} The number of elements in this queue.
+   */
   get length():number;
 
+  /**
+   * Returns true if this queue contains no elements.
+   * @return {boolean} true if this queue contains no elements.
+   */
   isEmpty(): boolean;
 }
